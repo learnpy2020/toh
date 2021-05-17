8072ec640026680602c1651c4fe443b1d187bb98
BabuK <learnpython202020@gmail.com>
2021-05-17 16:42:22 +0530

    FInalCopy

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 31, name: 'Dr Nice' },
+    { id: 32, name: 'Narco' },
+    { id: 33, name: 'Bombasto' },
+    { id: 34, name: 'Celeritas' },
+    { id: 35, name: 'Magneta' },
+    { id: 36, name: 'RubberMan' },
+    { id: 37, name: 'Dynama' },
+    { id: 38, name: 'Dr IQ' },
+    { id: 39, name: 'Magma' },
+    { id: 40, name: 'Tornado' }
+];
